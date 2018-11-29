f6802a88709cc9ae43919d98cdc82cd33261ce6e
github-paperschool <dominic.jomaa@gmail.com>
2018-11-29 11:05:16 +0000

    modfiying docker login build tag and push script

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -3,15 +3,18 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-
     TAG="latest"
-
-else
-
+elif [ "$TRAVIS_BRANCH" != "master" ]; then
     TAG="$TRAVIS_BRANCH"
-
+else
+    TAG="latest"
 fi
 
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+echo "Building Docker Image"
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG .
 
-docker push $TRAVIS_REPO_SLUG
+echo "Tagging Docker Image"
+docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$TRAVIS_REPO_SLUG
+
+echo "Pushing Docker Image"
+docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
